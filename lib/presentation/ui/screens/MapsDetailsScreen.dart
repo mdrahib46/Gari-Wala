c4dfafc6f0514c2fcc609c1f0e6d781299035f01
mdrahib46 <nahid179846@bsdi-bd.org>
2025-04-11 21:17:28 +0600

    Code Refactor of optimization

--- lib/presentation/ui/screens/MapsDetailsScreen.dart
+++ lib/presentation/ui/screens/MapsDetailsScreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:gariwala/data/model/car_models.dart' show Car;
+import 'package:gariwala/presentation/ui/screens/car_list_screen.dart';
+import 'package:gariwala/utils/cars.dart';
 import 'package:latlong2/latlong.dart';
 
 class MapsDetailsScreen extends StatelessWidget {
@@ -9,11 +11,7 @@ class MapsDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Example car for demonstration. You can pass this dynamically as needed.
-    final Car demoCar = Car(
-      model: 'Toyota Corolla',
-      distance: 120,
-      fuelCapacity: 45, pricePerHour: 45,
-    );
+  final CarsList car;
 
     return Scaffold(
       extendBodyBehindAppBar: true,
@@ -42,73 +40,13 @@ class MapsDetailsScreen extends StatelessWidget {
           ),
           Align(
             alignment: Alignment.bottomCenter,
-            child: carDetailCard(demoCar),
+            child: carDetailCard(CarsList.cars[0]),
           ),
         ],
       ),
     );
   }
 }
-
-
-// Widget carDetailCard() {
-//   return SizedBox(
-//     height: 350,
-//     child: Stack(
-//       children: [
-//         Container(
-//           padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-//           width: double.infinity,
-//           decoration: BoxDecoration(
-//             color: Colors.black54,
-//             borderRadius: BorderRadius.only(
-//               topLeft: Radius.circular(30),
-//               topRight: Radius.circular(30),
-//             ),
-//             boxShadow: [
-//               BoxShadow(color: Colors.black38, spreadRadius: 0, blurRadius: 10),
-//             ],
-//           ),
-//           child: Column(
-//             children: [
-//               SizedBox(height: 20),
-//               Text(
-//                 'car.model',
-//                 style: TextStyle(
-//                   fontSize: 24,
-//                   fontWeight: FontWeight.bold,
-//                   color: Colors.white,
-//                 ),
-//               ),
-//               const SizedBox(height: 10),
-//               Row(
-//                 children: [
-//                   Icon(Icons.directions_car, color: Colors.white, size: 16),
-//                   SizedBox(width: 8),
-//                   Text(
-//                     '> {car.distance} km',
-//                     style: TextStyle(color: Colors.white, fontSize: 14),
-//                   ),
-//                   SizedBox(width: 10),
-//                   Icon(Icons.battery_full, color: Colors.white, size: 14),
-//                   Text(
-//                     'car.fuelCapacity',
-//                     style: TextStyle(color: Colors.white, fontSize: 14),
-//                   ),
-//                   SizedBox(width: 10),
-//
-//                 ],
-//               ),
-//             ],
-//           ),
-//
-//
-//         ),
-//       ],
-//     ),
-//
-//   );
-// }
 
 Widget carDetailCard(Car car) {
   return Container(
