8ecf3e0bf83ead41d9bcb120d125d58954cdedc8
mdrahib46 <nahid179846@bsdi-bd.org>
2025-04-18 14:04:28 +0600

    Animation has been intregrated

--- lib/presentation/ui/screens/MapsDetailsScreen.dart
+++ lib/presentation/ui/screens/MapsDetailsScreen.dart
@@ -4,10 +4,34 @@ import 'package:gariwala/data/model/car_models.dart';
 import 'package:gariwala/presentation/widgets/carDetailsCard.dart';
 import 'package:latlong2/latlong.dart';
 
-class MapsDetailsScreen extends StatelessWidget {
+class MapsDetailsScreen extends StatefulWidget {
   const MapsDetailsScreen({super.key, required this.car});
 
   final Car car;
+
+  @override
+  State<MapsDetailsScreen> createState() => _MapsDetailsScreenState();
+}
+
+class _MapsDetailsScreenState extends State<MapsDetailsScreen> with TickerProviderStateMixin{
+
+  AnimationController? _animationController;
+  Animation<double>? _animation;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(duration: Duration(seconds: 3), vsync: this);
+
+    _animation = Tween<double>(begin: 1.0, end: 1.5).animate(_animationController!)..addListener((){
+      setState(() {
+
+      });
+    });
+
+   _animationController?.forward();
+    super.initState();
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -38,12 +62,14 @@ class MapsDetailsScreen extends StatelessWidget {
           ),
           Align(
             alignment: Alignment.bottomCenter,
-            child: carDetailCard(car: car),
+            child: carDetailCard(car: widget.car),
           ),
         ],
       ),
     );
   }
+
+  
 }
 
 
